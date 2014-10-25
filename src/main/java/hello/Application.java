5d1df6741a3a843870e8ee474783aff752f75755
ironvital <ironvital7@gmail.com>
2014-10-26 01:01:46 +0300

    Project structure

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableAsync
+public class Application {
+
+}
