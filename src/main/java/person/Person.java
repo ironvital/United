69abff4f85124ff41d4ca16e5826095c34285532
funkysprings <miwa-93@mail.ru>
2014-11-08 21:35:06 +0200

    Created person's class, rep and controller

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,34 @@
+package person;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private int phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+}
